96cf36581b99b5e4bc3258becdf483551545bdd0
Pid24 <rofidnasifannafie@gmail.com>
2024-04-11 06:21:21 +0700

    perbandingan query builder and eloquent

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,11 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function index() {
-        $student = Student::all();
-        return view('student', ['studentList' => $student]);
+        // $student = Student::all();
+        // return view('student', ['studentList' => $student]);
+
+        // Query Builder
+        // $student = DB::table('students')->get();
+        // DB::table('students')->insert([
+        //     'name' => 'Jefri Nichol',
+        //     'gender' => 'L',
+        //     'nis' => '1152000001',
+        //     'class_id' => 1
+        // ]);
+        // DB::table('students')->where('id', 26)->update([
+        //     'name' => 'Adipati Dolken'
+        // ]);
+        // DB::table('students')->where('id', 26)->delete();
+
+        // Eloquent
+        // $student = Student::all();
+        // Student::create([
+        //     'name' => 'Anatasia Kosasih',
+        //     'gender' => 'p',
+        //     'nis' => '1152000004',
+        //     'class_id' => 2
+        // ]);
+        // Student::find(27)->update([
+        //     'name' => 'Astrid Qanitah Arioputri',
+        //     'nis' => '1152000043'
+        // ]);
+        Student::find(27)->delete();
     }
 }
